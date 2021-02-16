4fb1de18a957427267f3646618073e0f64a5b426
wisnusabda <manusiq630@gmail.com.com>
2021-02-16 21:59:22 +0800

    Commit

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -34,17 +34,18 @@
       </ion-fab>
       <ExploreContainer name="Tab 2 page" />
     </ion-content>
+    
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent,  } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 import { add, arrowUpCircle, alertOutline, } from 'ionicons/icons';
 
 export default  {
   name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, },
   setup() {
     return {
       add,
--- src/views/start.vue
+++ src/views/start.vue
@@ -24,8 +24,22 @@
 </template>
 
 <script>
+import { alertController, IonButton } from '@ionic/vue'
 export default {
-
+  components: {IonButton},
+  methods: {
+    async presentAlertMultipleButtons() {
+      const alert = await alertController
+      .create({
+        cssClass: 'my-custom-class',
+        header: 'Alert',
+        subHeader: 'Subtitle',
+        message: 'This is alert message',
+        buttons: ['Cancel', 'Open Modal', 'Delete'],
+      });
+      return alert.present();
+    }
+  }
 }
 </script>
 
